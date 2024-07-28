c12fd15c0a4b97262cb2c99c3e0803d88539e3ff
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-07-28 11:25:09 +0300

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      colors: {
+        'secondary': '#003366',
+        'cards':'#116696',
+        'gray-100': '#f5f5f5',
+        'base': '#0D6D82', 
+        'bg': '#000d1a',
+         },
+    },
+  },
+  plugins: [],
+}
