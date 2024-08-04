9c3acb09c8539cbc0fb93de18938123b2197f066
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-08-04 13:55:25 +0300

    login and reg routess

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/home'
+import Login from './Pages/login';
+import Register from './Pages/register';
 import './index.css'
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <div className="flex flex-col bg-gray-900">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </div>
       </Router>
