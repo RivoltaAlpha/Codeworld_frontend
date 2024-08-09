f4c21c057500be1552d01ee724c990991ccd28a5
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-08-10 00:39:34 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/home'
 import Login from './Pages/login';
 import Register from './Pages/register';
+import UserProjectsList from './user/projects';
+import Dashboard from './user/dashboard';
 import './index.css'
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/projects" element={<UserProjectsList />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
       </Router>
