20ca35040a2cdbc221e435706fc401601f404f3d
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-08-10 00:40:29 +0300

    graphs

---
+++ src/components/graphSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GraphSection: React.FC = () => {
+  return (
+    <div className="w-full h-64 bg-cards rounded shadow p-4">
+      <h2 className="text-lg font-bold">Graphs depicting the progress of projects</h2>
+      {/* Graph component would go here */}
+    </div>
+  );
+};
+
+export default GraphSection;
