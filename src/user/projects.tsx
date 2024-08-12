7da6d028d4be774f54cc946af4a8c980f8dc5141
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-08-12 14:02:24 +0300

    user

--- src/user/projects.tsx
+++ src/user/projects.tsx
@@ -1,37 +1,29 @@
-// components/UserProjectsList.tsx
 import React from 'react';
 import projectsApi from '../features/projects/projectsAPI';
-import { Link } from 'react-router-dom';
-import { Project } from '../types/types';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../app/store';
+import ProjectCard from '../components/projectCard';
+import { useNavigate } from 'react-router-dom';
 
-interface ProjectItemProps {
-  project: Project;
-}
-
-const ProjectItem: React.FC<ProjectItemProps> = ({ project }) => {
-  return (
-    <div className="border p-4 rounded">
-      <h3 className="text-xl font-semibold">{project.project_name}</h3>
-      <p>{project.description}</p>
-      <p>Status: {project.project_status}</p>
-      <Link to={`/projects/${project.projects_id}`} className="text-blue-500 hover:underline">
-        View Details
-      </Link>
-    </div>
-  );
-};
 
 const UserProjectsList: React.FC = () => {
-  const { data: projects, isLoading, isError } = projectsApi.useGetUserProjectsQuery();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  if (isLoading) return <div>Loading projects...</div>;
+  const bookings = data?.[0]?.bookings || []; // Access nested bookings array
+  console.log(bookings);
+  const { user }: any = useSelector((state: RootState) => state.userAuth.user?.user_id && state.userAuth);
+  const userId = user?.user_id;
+  const { data: projects, isLoading, isError } = projectsApi.useGetUserProjectsQuery(userId);
+
+  if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading projects</div>;
 
   return (
     <div className="space-y-4">
       <h2 className="text-2xl font-bold">Your Projects</h2>
       {projects?.map((project) => (
-        <ProjectItem key={project.projects_id} project={project} />
+        <ProjectCard key={project.projects_id} />
       ))}
     </div>
   );
