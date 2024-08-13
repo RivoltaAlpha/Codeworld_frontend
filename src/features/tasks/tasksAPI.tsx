ada5983dfd4abce22a28daeea4fe7d8718da48cc
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-08-13 21:57:23 +0300

    queries

--- src/features/tasks/tasksAPI.tsx
+++ src/features/tasks/tasksAPI.tsx
@@ -1,44 +1,65 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Task } from '../../types/types';
 
-export const tasksApi = createApi({
-  reducerPath: 'tasksApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+export const tasksAPI = createApi({
+  reducerPath: 'tasksAPI',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
+
+  tagTypes: ['Tasks', 'Task'],
   endpoints: (builder) => ({
-    getTasks: builder.query({
+    getTasks: builder.query<Task[], void>({
       query: () => 'tasks',
+      providesTags: ['Tasks'],
     }),
-    getTask: builder.query<Task, string>({
-        query: (id) => `tasks/${id}`,
-      }),
-    createTask: builder.mutation({
-      query: (task) => ({
-        url: 'tasks',
+
+    getTask: builder.query<Task, number>({
+      query: (task_id) => `task/${task_id}`,
+    }),
+
+    createTask: builder.mutation<Task, Partial<Task>>({
+      query: (newTask) => ({
+        url: 'create-task',
         method: 'POST',
-        body: task,
+        body: newTask,
       }),
+      invalidatesTags: ['Tasks', 'Task'],
     }),
-    deleteTask: builder.mutation({
-      query: (task_id) => ({
-        url: `tasks/${task_id}`,
+
+    updateTask: builder.mutation<Task, { id: number; data: Partial<Task> }>({
+      query: ({ id, data }: { id: number; data: Partial<Task> }) => ({
+        url: `update-task/${id}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Tasks'],
+    }),
+
+    deleteTask: builder.mutation<{ success: boolean; id: number }, number>({
+      query: (id) => ({
+        url: `delete-task/${id}`,
         method: 'DELETE',
       }),
+      invalidatesTags: ['Tasks'],
     }),
-    updateTask: builder.mutation({
-      query: ({ task_id, ...task }) => ({
-        url: `tasks/${task_id}`,
+
+    getTasksByProjectId: builder.query<Task[], number>({
+      query: (projectId) => `tasks/project/${projectId}`,
+      providesTags: ['Tasks'],
+    }),
+
+    getTasksByUserId: builder.query<Task[], number>({
+      query: (userId) => `tasks/user/${userId}`,
+      providesTags: ['Tasks'],
+    }),
+    updateTaskStatus: builder.mutation <Task, { id: number; data: Partial<Task> }>({
+      query: ({ id, data }: { id: number; data: Partial<Task> }) => ({
+        url: `update-task-status/${id}`,
         method: 'PUT',
-        body: task,
+        body: data,
       }),
-    }),
-    updateTaskStatus: builder.mutation({
-      query: ({ task_id, status }) => ({
-        url: `tasks/${task_id}/status`,
-        method: 'PUT',
-        body: { status },
-      }),
-    }),
+      invalidatesTags: ['Tasks'],
+    })
   }),
 });
 
-export default tasksApi;
+export default tasksAPI;
