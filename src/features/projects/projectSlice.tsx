e28bef5c49cf72b332beb76f242a5433320c5380
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-08-14 11:55:33 +0300

    project slice states

--- src/features/projects/projectSlice.tsx
+++ src/features/projects/projectSlice.tsx
@@ -5,21 +5,24 @@ import { Project, ProjectsState } from '../../types/types';
 
 
 const initialState: ProjectsState = {
-  selectedProject: null,
-};
-
+  selectedProject:  JSON.parse(localStorage.getItem('selectedProject') || 'null'),
+  project:  JSON.parse(localStorage.getItem('project') || 'null'),
+}
 const projectsSlice = createSlice({
-  name: 'projects',
+  name: 'project',
   initialState,
   reducers: {
     setSelectedProject: (state, action: PayloadAction<Project | null>) => {
-      state.selectedProject = action.payload;
+      state.project = action.payload;
+      localStorage.setItem('selectedProject', JSON.stringify(action.payload));
     },
     updateSelectedProject (state, action: PayloadAction<Project>) {
       state.selectedProject = action.payload;
+      localStorage.setItem('selectedProject', JSON.stringify(action.payload));
     },
     clearSelectedProject (state) {
       state.selectedProject = null;
+      localStorage.removeItem('selectedProject');
     }
   },
 });
